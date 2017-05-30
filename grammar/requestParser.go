3e67e71f6e3a541eec2e13d3dfda9f4b97dad9ea
talbor49 <talbor49@gmail.com>
2017-05-31 02:34:22 +0300

    Made requestParser handle 2 bytes requests too

--- grammar/requestParser.go
+++ grammar/requestParser.go
@@ -9,19 +9,24 @@ import (
 func byte2dSliceToStringSlice(byteslice [][]byte) []string {
 	var stringslice []string
 	for _, bslice := range byteslice {
-		stringslice = append(stringslice, string(bslice))
+		if len(bslice) > 0 {
+			stringslice = append(stringslice, string(bslice))
+		}
 	}
 	return stringslice
 }
 
 func ParseRequest(data []byte) (Request, error) {
-	if len(data) < 3 {
+	if len(data) < 2 {
 		return Request{}, errors.New("Request is too short.")
 	}
 	requestType := data[0]
 	requestStatus := data[1]
-	rawData := data[2:]
-	tokens := byte2dSliceToStringSlice(bytes.Split(rawData, []byte{0}))
+	var tokens []string
+	if len(data) > 2 {
+		rawData := data[2:]
+		tokens = byte2dSliceToStringSlice(bytes.Split(rawData, []byte{0}))
+	}
 
 	request := Request{Type: requestType, Status: requestStatus, RequestData: tokens}
 
